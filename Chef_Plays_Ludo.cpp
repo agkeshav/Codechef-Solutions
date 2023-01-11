20ba0507bd7cb4d5f8703deae842a75693760426
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-01-11 22:18:17 +0530

    LUDO Added

---
+++ Chef_Plays_Ludo.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to return LCM of two numbers
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;cin>>x;
+        if(x==6){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+
+    return 0;
+}
